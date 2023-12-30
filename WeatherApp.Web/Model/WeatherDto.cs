b0f4e6406f071e965ac982d83e3c82c6f5997e61
tin hoang nhu <hoangnhutin@live.com>
2023-12-30 22:32:05 +0700

    fix front end

--- WeatherApp.Web/Model/WeatherDto.cs
+++ WeatherApp.Web/Model/WeatherDto.cs
@@ -2,11 +2,11 @@
 {
     public class WeatherDto
     {
-        public DateOnly Date { get; set; }
+        public string Date { get; set; }
 
-        public int TemperatureC { get; set; }
+        public string TemperatureC { get; set; }
 
-        public int TemperatureF {  get; set; }
+        public string TemperatureF {  get; set; }
 
         public string? Summary { get; set; }
     }
